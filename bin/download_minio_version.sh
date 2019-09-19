ddc214797781991ed819513d8901de37fc6d3bb9
Andrew Johnson <blackandred@users.noreply.github.com>
2019-09-19 09:00:35 +0200

    Fixed a typo in the URL for archive download

--- bin/download_minio_version.sh
+++ bin/download_minio_version.sh
@@ -3,5 +3,5 @@
 VERSION=$1
 
 echo " >> Downloading Minio Client in version '${VERSION}'"
-wget "https://dl.minio.io/client/mc/release/linux-amd64/mc.${VERSION}" -O /usr/bin/mc || wget "https://dl.minio.io/client/mc/release/linux-amd64/archive/${VERSION}" -O /usr/bin/mc || exit 1
+wget "https://dl.minio.io/client/mc/release/linux-amd64/mc.${VERSION}" -O /usr/bin/mc || wget "https://dl.minio.io/client/mc/release/linux-amd64/archive/mc.${VERSION}" -O /usr/bin/mc || exit 1
 chmod +x /usr/bin/mc
